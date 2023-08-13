c3ad45919cdb63e3a877925a36b62dec51661dca
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-08-13 07:15:58 -0700

    complete
    complete

--- MATH CALCULATION APP.js
+++ MATH CALCULATION APP.js
@@ -1,19 +1,25 @@
+
 var num1 = +prompt("ENTER YOUR 1 DIGIT")
 var num2 = +prompt("ENTER YOUR 2 DIGIT")
+
+
 
 
 var add = num1 + num2
 var sub = num1 - num2
 var mul = num1 * num2
 var div = num1 / num2
-
 document.write("THE ADDITION OF " +num1+ " AND " +num2+ " IS " +add)
 document.write("<br>")
+document.write("<br>")
 document.write("THE SUBTRACT OF " +num1+ " AND " +num2+ " IS " +sub)
+document.write("<br>")
 document.write("<br>")
 
 document.write("THE MULTIPLY OF " +num1+ " AND " +num2+ " IS " +mul)
 document.write("<br>")
+document.write("<br>")
 
 document.write("THE DIVISION OF " +num1+ " AND " +num2+ " IS " +div)
 document.write("<br>")
+
